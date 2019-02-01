6ea1cb948887b324b852910901e5fd2c2003a2bb
DefKorns <defkorns@gmail.com>
2019-02-01 09:56:51 -0100

    cleanup

--- modules/chrono/xtras/thumbnail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gStorage="$(hakchi findGameStorage)"
-find "$gStorage" -path "*_small.png*" | while read file; do
- [ -f "$file" ] && cp "$file" "$file"_backup && cp "/var/lib/hakchi/rootfs/usr/share/themes/snes/ghost/xtras/thumb.png" "$file"
- done
-
--- modules/chrono/xtras/thumbnailfix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gStorage="$(hakchi findGameStorage)"
-find "$gStorage" -path "*_small.png*" | while read -r file; do
- [ -f "$file"_backup ] && cp "$file"_backup "$file" && rm "$file"_backup
- done
-
